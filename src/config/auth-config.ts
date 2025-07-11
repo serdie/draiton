091328a432703f2e8f81bd0d67b36efea1d8295b
Diego Gómez Marín <serdiegm@gmail.com>
2025-07-11 23:49:21 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/config/auth-config.ts
+++ src/config/auth-config.ts
@@ -15,7 +15,7 @@ export const authConfig: AuthOptions = {
     serviceAccount: {
         projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
         clientEmail: process.env.FIREBASE_CLIENT_EMAIL!,
-        privateKey: process.env.FIREBASE_PRIVATE_KEY!.replace(/\\n/g, '\n'),
+        privateKey: (process.env.FIREBASE_PRIVATE_KEY || '').replace(/\\n/g, '\n'),
     },
     // Optional: customize the token claims
     // onValidToken: (token) => {
