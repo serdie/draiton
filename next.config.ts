b9ace5adf2f49a410151bba866482585038630f0
Diego Gómez Marín <serdiegm@gmail.com>
2025-09-02 10:10:08 +0000

    a la sección de finanzas inteligente de la home cambia la imagen actual

--- next.config.ts
+++ next.config.ts
@@ -27,6 +27,12 @@ const nextConfig: NextConfig = {
         hostname: 'picsum.photos',
         port: '',
         pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
       }
     ],
   },
