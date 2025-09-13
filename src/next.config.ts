36f643f96873a549a5104b6f4b37e9b3397b47ab
Diego Gómez Marín <serdiegm@gmail.com>
2025-09-13 22:28:42 +0000

    The app isn't starting. Please investigate what could be wrong based on

---
+++ src/next.config.ts
@@ -0,0 +1,51 @@
+
+import type {NextConfig} from 'next';
+
+const nextConfig: NextConfig = {
+  /* config options here */
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'placehold.co',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 's0.wordpress.com',
+        port: '',
+        pathname: '/mshots/v1/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'storage.googleapis.com',
+        port: '',
+        pathname: '/**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'www.diemy.es',
+        port: '',
+        pathname: '/**',
+      }
+    ],
+  },
+  experimental: {
+  },
+  allowedDevOrigins: ["https://*.cloudworkstations.dev"],
+};
+
+export default nextConfig;
