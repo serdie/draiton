53f3e9f6cc4c7d14cde80567c713a41171dc900f
Diego Gómez Marín <serdiegm@gmail.com>
2025-10-21 12:01:47 +0000

    The app isn't starting. Please investigate what could be wrong based on

--- next.config.ts
+++ next.config.ts
@@ -52,7 +52,7 @@ const nextConfig: NextConfig = {
         hostname: 'firebasestorage.googleapis.com',
         port: '',
         pathname: '/**',
-      },
+      }
     ],
   },
   experimental: {
